df452fd10a62b08b7d33c38a181018fc6279a30f
Oleg Efimov <efimovov@gmail.com>
2011-10-18 02:52:09 +0400

    Implement unpacker for unknown length containers, fixes #11, fixes #12

---
+++ test/test-unknown-length.js
@@ -0,0 +1,34 @@
+/*!
+ * Copyright by Oleg Efimov
+ *
+ * See license text in LICENSE file
+ */
+
+var fs = require('fs');
+var UBJSON = require('../lib/ubjson.js');
+
+// Create tests for all fixtures files
+var files = fs.readdirSync(__dirname + '/fixtures/unknown-length')
+              .filter(function(file) { return file.match(/\.json$/); }).sort();
+
+files.forEach(function(file) {
+  var dataType = file.replace(/\.json$/, '').replace(/_/g, '/');
+
+  var fileJSON = __dirname + '/fixtures/unknown-length/' + file;
+  var fileUBJSON = fileJSON.replace(/\.json$/, '.ubj');
+
+  module.exports[dataType] = function (test) {
+    test.expect(1);
+
+    var jsonBuffer = fs.readFileSync(fileJSON);
+    var ubjsonBuffer = fs.readFileSync(fileUBJSON);
+
+    var jsonObject = JSON.parse(jsonBuffer.toString('utf8'));
+
+    UBJSON.unpack(ubjsonBuffer, function (object) {
+      test.deepEqual(object, jsonObject, 'UBJSON.unpack(' + dataType + ')');
+
+      test.done();
+    });
+  };
+});
